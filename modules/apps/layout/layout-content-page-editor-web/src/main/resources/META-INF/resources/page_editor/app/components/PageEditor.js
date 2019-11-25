19ad44d9fe5eb33557238ccfc8f0225cb49e898f
Diego Nascimento <diego.nascimento@liferay.com>
2019-11-25 13:58:36 -0300

    LPS-103814 Adds comments about showFloatingToolbar action

--- modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/components/PageEditor.js
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/components/PageEditor.js
@@ -129,6 +129,11 @@ function Container({children, item}) {
 				onClick={event => {
 					event.preventDefault();
 	
+					/**
+					 * Mocked buttons to FloatingToolbar. However, the mechanism needs
+					 * just dispatch an action with previously-defined-constants to render
+					 * FloatingToolbar buttons.
+					 */
 					dispatch(
 						showFloatingToolbar({
 							buttons: [
@@ -263,6 +268,11 @@ function Fragment({item}) {
 				onClick={event => {
 					event.preventDefault();
 
+					/**
+					 * Mocked buttons to FloatingToolbar. However, the mechanism needs
+					 * just dispatch an action with previously-defined-constants to render
+					 * FloatingToolbar buttons.
+					 */
 					dispatch(
 						showFloatingToolbar({
 							buttons: [
