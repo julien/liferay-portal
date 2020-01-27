516f18a442a655dc58b58858c966edbcff91faf7
Diego Nascimento <diego.nascimento@liferay.com>
2020-01-27 20:22:18 -0300

    wip

--- modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/components/layout-data-items/Column.js
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/components/layout-data-items/Column.js
@@ -12,28 +12,178 @@
  * details.
  */
 
+import ClayButton from '@clayui/button';
+
+/* eslint no-console: 0 */
 import classNames from 'classnames';
-import React from 'react';
+import React, {useContext, useEffect, useMemo, useRef, useState} from 'react';
 
 import {LAYOUT_DATA_ITEM_DEFAULT_CONFIGURATIONS} from '../../config/constants/layoutDataItemDefaultConfigurations';
 import {LAYOUT_DATA_ITEM_TYPES} from '../../config/constants/layoutDataItemTypes';
+import {useSelector} from '../../store/index';
+import ResizingContext from './ResizingContext';
 
-const Column = React.forwardRef(({children, className, item}, ref) => {
-	const size =
-		item.config.size ||
-		LAYOUT_DATA_ITEM_DEFAULT_CONFIGURATIONS[LAYOUT_DATA_ITEM_TYPES.column]
-			.size;
+function getColumnInfo({item, layoutData}) {
+	const rowColumns = layoutData.items[item.parentId].children;
+	const colIndex = rowColumns.indexOf(item.itemId);
+	const nextColumnIndex = colIndex + 1;
+	const nextColumn = {...layoutData.items[rowColumns[nextColumnIndex]]};
+	const nextColumnConfig =
+		typeof nextColumn === 'object' && Object.keys(nextColumn).length
+			? nextColumn.config
+			: {};
 
-	return (
-		<div
-			className={classNames(className, 'col', {
-				[`col-${size}`]: size
-			})}
-			ref={ref}
-		>
-			{children}
-		</div>
+	return {
+		colIndex,
+		isLastColumn: rowColumns.indexOf(item.itemId) === rowColumns.length - 1,
+		nextColumn: nextColumn ? nextColumn : {},
+		nextColumnConfig: nextColumn ? nextColumnConfig : {},
+		nextColumnIndex: colIndex + 1,
+		rowColumns
+	};
+}
+
+function getColumnsPosition({columnElement, nextColumnElement}) {
+	let maxPosition = 0;
+	let minPosition = 0;
+
+	if (columnElement && nextColumnElement) {
+		const nextColumnRect = nextColumnElement.getBoundingClientRect();
+		maxPosition = nextColumnRect.left + nextColumnRect.width;
+		minPosition = columnElement.getBoundingClientRect().left;
+	}
+
+	return {maxPosition, minPosition};
+}
+
+function calculateColumnsSizes({
+	currentColumnSize,
+	maxPosition,
+	minPosition,
+	mousePosition,
+	nextColumnSize
+}) {
+	const position = Math.max(
+		Math.min(mousePosition, maxPosition),
+		minPosition
 	);
-});
+	const maxColumns =
+		(parseInt(currentColumnSize, 10) || 1) +
+		(parseInt(nextColumnSize, 10) || 1) -
+		1;
+	const newCurrentColumns = Math.max(
+		Math.round(
+			((position - minPosition) / (maxPosition - minPosition)) *
+				maxColumns
+		),
+		1
+	);
+	return {maxColumns, newCurrentColumns};
+}
+
+const Column = React.forwardRef(
+	({children, className, item, ...props}, ref) => {
+		const {
+			config: {
+				size = LAYOUT_DATA_ITEM_DEFAULT_CONFIGURATIONS[
+					LAYOUT_DATA_ITEM_TYPES.column
+				].size
+			},
+			itemId
+		} = item;
+
+		const layoutData = useSelector(state => state.layoutData);
+
+		const {
+			mouseX,
+			onColumnSizeChanged,
+			onResizeHandlerMouseDown,
+			resizing
+		} = useContext(ResizingContext);
+		const columnRef = useRef(null);
+
+		const {
+			colIndex,
+			isLastColumn,
+			nextColumn,
+			nextColumnConfig,
+			nextColumnIndex
+		} = useMemo(
+			() =>
+				getColumnInfo({
+					item,
+					layoutData
+				}),
+			[item, layoutData]
+		);
+
+		const [columnsSize, setColumnsSize] = useState({
+			current: size,
+			next: nextColumnConfig.size
+		});
+
+		const columnElement = columnRef.current;
+		const nextColumnElement = document.getElementById(
+			`page_editor-col-${nextColumnIndex}`
+		);
+
+		const {maxPosition, minPosition} = useMemo(
+			() => getColumnsPosition({columnElement, nextColumnElement}),
+			[columnElement, nextColumnElement]
+		);
+
+		useEffect(() => {
+			if (resizing) {
+				const {maxColumns, newCurrentColumns} = calculateColumnsSizes({
+					currentColumnSize: columnsSize.current,
+					maxPosition,
+					minPosition,
+					mousePosition: mouseX,
+					nextColumnSize: columnsSize.next
+				});
+
+				setColumnsSize({
+					current: newCurrentColumns,
+					next: maxColumns - newCurrentColumns + 1
+				});
+			}
+		}, [columnsSize, maxPosition, minPosition, mouseX, resizing]);
+
+		useEffect(
+			() => onColumnSizeChanged({columnsSize, itemId, nextColumn}),
+			[columnsSize, itemId, nextColumn, onColumnSizeChanged]
+		);
+
+		const resizeHandler = (
+			<div>
+				{children}
+				<ClayButton
+					className="page-editor__col-resizer"
+					onMouseDown={onResizeHandlerMouseDown}
+				/>
+			</div>
+		);
+
+		return (
+			<>
+				<div
+					className={classNames(className, 'col', {
+						[`col-${size}`]: size
+					})}
+					id={`page_editor-col-${colIndex}`}
+					ref={node => {
+						columnRef.current = node;
+						if (ref) {
+							ref.current = node;
+						}
+					}}
+					{...props}
+				>
+					{!isLastColumn ? resizeHandler : children}
+				</div>
+			</>
+		);
+	}
+);
 
 export default Column;
--- modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/components/layout-data-items/Row.js
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/components/layout-data-items/Row.js
@@ -12,19 +12,137 @@
  * details.
  */
 
+/* eslint no-console: 0 */
 import classNames from 'classnames';
-import React from 'react';
+import {useEventListener} from 'frontend-js-react-web';
+import dom from 'metal-dom';
+import React, {
+	useCallback,
+	useContext,
+	useEffect,
+	useRef,
+	useState
+} from 'react';
 
+import useMousePosition from '../../../core/hooks/useMousePosition';
+import updateColSize from '../../actions/updateColSize';
 import {LAYOUT_DATA_ITEM_DEFAULT_CONFIGURATIONS} from '../../config/constants/layoutDataItemDefaultConfigurations';
 import {LAYOUT_DATA_ITEM_TYPES} from '../../config/constants/layoutDataItemTypes';
+import {ConfigContext} from '../../config/index';
+import {useDispatch, useSelector} from '../../store/index';
+import resizeColumns from '../../thunks/resizeColumns';
+import ColumnOverlayGrid from './ColumnOverlayGrid';
+import ResizingContext from './ResizingContext';
+
+const TOTAL_NUMBER_OF_COLUMNS = 12;
+
+function getGridDeltas(colWidth) {
+	return [...Array(TOTAL_NUMBER_OF_COLUMNS).keys()].reduce(
+		(acc, current, index) => {
+			if (index === 0) {
+				return acc;
+			}
+
+			return [...acc, acc[current - 1] + colWidth];
+		},
+		[colWidth]
+	);
+}
+
+function getCurrentGridPosition(mousePosition, gridSizes) {
+	const indexArr = gridSizes.map(size => Math.abs(size - mousePosition));
+
+	const min = Math.min.apply(Math, indexArr);
+
+	return indexArr.indexOf(min);
+}
 
 const Row = React.forwardRef(({children, className, item, layoutData}, ref) => {
 	const {gutters} = {
 		...LAYOUT_DATA_ITEM_DEFAULT_CONFIGURATIONS[LAYOUT_DATA_ITEM_TYPES.row],
 		...item.config
 	};
-
 	const parent = layoutData.items[item.parentId];
+	const config = useContext(ConfigContext);
+	const dispatch = useDispatch();
+	const store = useSelector(state => state);
+	const {mouseX} = useMousePosition();
+	const rowRef = useRef(null);
+
+	const [gridSizes, setGridSizes] = useState([]);
+	const [highlightedColumn, setHighLightedColumn] = useState(0);
+	const [resizing, setResizing] = useState(false);
+
+	useEffect(() => {
+		if (!rowRef.current) {
+			return;
+		}
+
+		const rowWidth = rowRef.current.offsetWidth;
+		const colWidth = Math.floor(rowWidth / TOTAL_NUMBER_OF_COLUMNS);
+		const sizes = getGridDeltas(colWidth);
+		// offsetLeft
+		setGridSizes(sizes);
+	}, [rowRef]);
+
+	useEffect(() => {
+		if (!resizing) {
+			return;
+		}
+
+		const containerRef = dom.closest(
+			rowRef.current,
+			'.container, .container-fluid'
+		);
+
+		const mousePosition = mouseX - containerRef.offsetLeft;
+
+		const index = getCurrentGridPosition(mousePosition, gridSizes);
+
+		setHighLightedColumn(index);
+	}, [gridSizes, resizing, mouseX]);
+
+	function clearResizing() {
+		if (resizing) {
+			setResizing(false);
+			setHighLightedColumn(0);
+		}
+	}
+
+	function handleMouseLeave() {
+		clearResizing();
+	}
+
+	const onResizeHandlerMouseDown = useCallback(() => setResizing(true), []);
+
+	function onResizeHandlerMouseUp() {
+		clearResizing();
+
+		resizeColumns({
+			config,
+			layoutData: store.layoutData,
+			store
+		});
+	}
+
+	const onColumnSizeChanged = useCallback(
+		({columnsSize, itemId, nextColumn}) => {
+			if (resizing) {
+				dispatch(
+					updateColSize({
+						itemId,
+						nextColumnItemId: nextColumn.itemId,
+						nextColumnSize: columnsSize.next,
+						size: columnsSize.current
+					})
+				);
+			}
+		},
+		[resizing, dispatch]
+	);
+
+	useEventListener('mouseup', onResizeHandlerMouseUp, false, document.body);
+	useEventListener('mouseleave', handleMouseLeave, false, document.body);
 
 	const rowContent = (
 		<div
@@ -34,9 +152,33 @@ const Row = React.forwardRef(({children, className, item, layoutData}, ref) => {
 				),
 				'no-gutters': !gutters
 			})}
-			ref={ref}
+			ref={node => {
+				if (node) {
+					rowRef.current = node;
+
+					if (ref) {
+						ref.current = node;
+					}
+				}
+			}}
 		>
-			{children}
+			<ResizingContext.Provider
+				value={{
+					mouseX,
+					onColumnSizeChanged,
+					onResizeHandlerMouseDown,
+					resizing,
+					rowRef
+				}}
+			>
+				{children}
+			</ResizingContext.Provider>
+			{resizing && (
+				<ColumnOverlayGrid
+					columnSpacing={gutters}
+					highlightedColumn={highlightedColumn}
+				/>
+			)}
 		</div>
 	);
 
